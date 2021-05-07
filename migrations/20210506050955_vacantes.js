487b7ea6ab77944f6ccd8e9c9c59964ab073e112
Adrian Herrera <rovskyhp@gmail.com>
2021-05-06 23:25:57 -0500

    Update de migraciones, hacia falta la relacion de empresa con vacantes

--- migrations/20210506050955_vacantes.js
+++ migrations/20210506050955_vacantes.js
@@ -1,16 +1,19 @@
 
 exports.up = (knex, Promise) => {
-    return knex.schema.createTable('vacantes', (table) => {
-        table.increments('id_vacante').primary();
-        table.string('titulo', 255).notNullable();
-        table.text('descripcion').notNullable();
-        table.decimal('salario',14,2);
-        table.string('tipo_de_trabajo', 255).notNullable();
-        table.boolean('estatus').notNullable().defaultTo(true);
-        table.timestamp('created_at').defaultTo(knex.fn.now());
-    });
+	return knex.schema.createTable('vacantes', (table) => {
+		table.increments('id_vacante').primary();
+		table.string('titulo', 255).notNullable();
+		table.text('descripcion').notNullable();
+		table.decimal('salario',14,2);
+		table.string('tipo_de_trabajo', 255).notNullable();
+		table.boolean('estatus').notNullable().defaultTo(true);
+		table.timestamp('created_at').defaultTo(knex.fn.now());
+		table.foreign('id_vacante')
+		.references('id_empresa')
+		.inTable('empresas');
+	});
 };
 
 exports.down = (knex, Promise) => {
-    return knex.schema.dropTableIfExists('vacantes');
+	return knex.schema.dropTableIfExists('vacantes');
 };
